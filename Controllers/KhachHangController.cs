f68200ce7d834dbae78be2da78f7bf971b97f7c9
bvhug <trvhung168@gmail.com>
2023-12-28 00:41:05 +0700

    update

--- Controllers/KhachHangController.cs
+++ Controllers/KhachHangController.cs
@@ -24,6 +24,53 @@ namespace Nhom2.Controllers
             _context = context;
         }
         // GET: KhachHang
+           public async Task<IActionResult> Upload()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult>Upload(IFormFile file)
+        {
+            if (file!=null)
+            {
+                string fileExtension = Path.GetExtension(file.FileName);
+                if (fileExtension != ".xls" && fileExtension != ".xlsx")
+                {
+                    ModelState.AddModelError("", "Please choose excel file to upload!");
+                }
+                else
+                {
+                    //rename file when upload to sever
+                    var fileName = DateTime.Now.ToShortTimeString() + fileExtension;
+                    var filePath = Path.Combine(Directory.GetCurrentDirectory() + "/Uploads/Excels", fileName);
+                    var fileLocation = new FileInfo(filePath).ToString();
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        //save file to server
+                        await file.CopyToAsync(stream);
+                        //read data from file and write to database
+                        var dt = _excelProcess.ExcelToDataTable(fileLocation);
+                        //dùng vòng lặp for để đọc dữ liệu dạng hd
+                        for (int i = 0; i < dt.Rows.Count; i++)
+                        {
+                            //create a new Student object
+                            var kh = new KhachHangModel();
+                            //set values for attribiutes
+                            kh.MaKhachHang = dt.Rows[i][0].ToString();
+                            kh.TenKhachHang = dt.Rows[i][1].ToString();
+                            //add oject to context
+                            _context.KhachHangModel.Add(kh);
+                        }
+                        //save to database
+                        await _context.SaveChangesAsync();
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+            }
+            return View();
+        
+    }
         public async Task<IActionResult> Index()
         {
             var applicationDbContext = _context.KhachHangModel.Include(k => k.GioiTinh);
